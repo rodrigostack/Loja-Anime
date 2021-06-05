608d27d557da30240ec00f1189c3f45934a8d896
Rodrigo Oliveira <frejat.rodrigo@gmail.com>
2021-06-05 12:32:19 -0300

    segunda versao

---
+++ src/header/header.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import './header.css'
+import Logo from '../imagens/logo.png'
+import Persona from '../imagens/naruto.png'
+
+export default function header(){
+    return(
+        <div className="container-fluid containerdefault">
+            <div className="row ">
+                <div className="col-md-4 col-sm-2 divcolunm">
+                    <img src={Logo} className="img-fluid "/>
+                </div>
+                <div className="col-md-5 divbetween">
+                    <h1 className="h1">Loja de Fã para Fã.</h1><br/>
+                    <p className="text-start">Temos todos so artigos de animes e series Tokusatsus</p><br/>
+                    <p className="text-center">Seja bem vindo(a) a HUNTER ANIMES</p>
+                </div>
+                <div className="col-md-3 divright">
+                    <img src={Persona} className="img-fluid"/>
+                </div>
+
+            </div>
+                
+        </div>
+    )
+}
