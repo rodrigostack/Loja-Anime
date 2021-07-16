35a1d7020abc68ee6fbfe77e660f23f4ec3278b5
Rodrigo Oliveira <ferjat.rodrigo@gmail.com>
2021-07-16 18:06:47 -0300

    versao com formularia cadastre-se

--- src/cadastro/cadastro.jsx
+++ src/cadastro/cadastro.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 export default function cadastro(){
     return(
-        <div>
-            <h2>FOrmulario</h2>
-        </div>
+      <div className="container">
+        <form>
+         <div className="row"> 
+            <div className="col-md-6"> 
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingname" placeholder="Digite seu nome"/>
+                <label for="floatingname">Nome Completo:</label>
+            </div>
+            </div> 
+            <div className="col-md-6">    
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="floatingemail" placeholder="Digite seu E-mail"/>
+                    <label for="floatingemail">Email:</label>
+                </div>
+            </div>
+         </div>
+         </form>   
+      </div>
     )
 }
