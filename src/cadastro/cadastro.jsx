ba68e87380f91f1c1628589d6996333518faa933
Rodrigo Oliveira <frejat.rodrigo@gmail.com>
2021-07-19 15:47:51 -0300

    puxando cep

--- src/cadastro/cadastro.jsx
+++ src/cadastro/cadastro.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import './cadastro.css'
 import InputMask from 'react-input-mask'
 import Axios from 'axios'
 
-export default function cadastro(){
+export default function Cadastro(){
+    const[nome,setNome] = useState();
+    const[email,setEmail] = useState();
+    const[tel,setTel] = useState();
+    const[cpf,setCpf] = useState();
+    const[cep,setCep] = useState("");
+    const[estado,setEstado] = useState();
+    const[cidade,setCidade] = useState();
+    const[bairro,setBairro] = useState();
+    const[endereco,setEndereco] = useState();
+
+    useEffect(()=>{
+        if (cep.length > 8) {
+           Axios.get(`http://viacep.com.br/ws/${cep}/json/`)
+           .then((resposta)=>{
+               setEndereco(resposta.data.logradouro);
+               setCidade(resposta.data.localidade);
+               setBairro(resposta.data.bairro)
+               setEstado(resposta.data.uf)
+           }) 
+        }
+    },[cep])
+
     return(
       <div className="container cadastrodefault">
           <div className="row" id="titlehead">
@@ -13,13 +35,13 @@ export default function cadastro(){
          <div className="row"> 
             <div className="col-md-6"> 
                 <div className="form-floating mb-3">
-                    <input type="text" className="form-control" id="floatingname" placeholder="Digite seu nome"/>
+                    <input type="text" className="form-control" id="floatingname" placeholder="Digite seu nome" name="nome" value={nome} onChange={e => setNome(e.target.value)}/>
                     <label for="floatingname">Nome Completo:</label>
                 </div>
             </div> 
             <div className="col-md-6">    
                 <div className="form-floating mb-3">
-                    <input type="email" className="form-control" id="floatingemail" placeholder="Digite seu E-mail"/>
+                    <input type="email" className="form-control" id="floatingemail" placeholder="Digite seu E-mail" name="email" value={email} onChange={e=>setEmail(e.target.value)}/>
                     <label for="floatingemail">Email:</label>
                 </div>
             </div>
@@ -27,13 +49,13 @@ export default function cadastro(){
          <div className="row"> 
             <div className="col-md-6"> 
                 <div className="form-floating mb-3">
-                    <InputMask mask="(99)99999-9999" type="text" className="form-control" id="floatingtel" placeholder="Digite seu Telefone"/>
+                    <InputMask mask="(99)99999-9999" type="text" className="form-control" id="floatingtel" placeholder="Digite seu Telefone" name="tel" value={tel} onChange={e=>setTel(e.target.value)}/>
                     <label for="floatingtel">Telefone:</label>
                 </div>
             </div> 
             <div className="col-md-6">    
                 <div className="form-floating mb-3">
-                    <InputMask mask="999.999.999-99" type="text" className="form-control" id="floatingcpf" placeholder="Digite seu CPF"/>
+                    <InputMask mask="999.999.999-99" type="text" className="form-control" id="floatingcpf" placeholder="Digite seu CPF" name="cpf" value={cpf} onChange={e=>setCpf(e.target.value)}/>
                     <label for="floatingcpf">CPF:</label>
                 </div>
             </div>
@@ -41,19 +63,19 @@ export default function cadastro(){
          <div className="row">
             <div className="col-md-2">    
                 <div className="form-floating mb-3">
-                    <InputMask mask="99999-999" type="text" className="form-control" id="floatingcep" placeholder="Digite seu CEP"/>
+                    <InputMask mask="99999-999" type="text" className="form-control" id="floatingcep" placeholder="Digite seu CEP" name="cep" value={cep} onChange={e=>setCep(e.target.value)}/>
                     <label for="floatingcep">CEP</label>
                 </div>
             </div>
             <div className="col-md-4">    
                 <div className="form-floating mb-3">
-                    <input type="text" className="form-control" id="floatingestado" placeholder="Digite seu estado"/>
+                    <input type="text" className="form-control" id="floatingestado" placeholder="Digite seu estado" name="estado" value={estado} onChange={e=>setEstado(e.target.value)}/>
                     <label for="floatingestado">Estado:</label>
                 </div>
             </div>
             <div className="col-md-6">    
                 <div className="form-floating mb-3">
-                    <input type="text" className="form-control" id="floatingcidade" placeholder="Digite seu cidade"/>
+                    <input type="text" className="form-control" id="floatingcidade" placeholder="Digite seu cidade" name="cidade" value={cidade} onChange={e=>setCidade(e.target.value)}/>
                     <label for="floatingcidade">Cidade:</label>
                 </div>
             </div>
@@ -61,13 +83,13 @@ export default function cadastro(){
          <div className="row">
             <div className="col-md-6"> 
                 <div className="form-floating mb-3">
-                    <input type="text" className="form-control" id="floatingbairro" placeholder="Digite seu bairro"/>
+                    <input type="text" className="form-control" id="floatingbairro" placeholder="Digite seu bairro" name="bairro" value={bairro} onChange={e=>setBairro(e.target.value)}/>
                     <label for="floatingbairro">Bairro:</label>
                </div> 
             </div> 
             <div className="col-md-6"> 
                 <div className="form-floating mb-3">
-                    <input type="text" className="form-control" id="floatingend" placeholder="Digite seu Endereço"/>
+                    <input type="text" className="form-control" id="floatingend" placeholder="Digite seu Endereço" name="endereco" value={endereco} onChange={e=>setEndereco(e.target.value)}/>
                     <label for="floatingend">Endereço:</label>
                </div> 
             </div>     
